38776d1a4c909646e1fa11b4ed68b12e5072aa9e
Doniyor Ganiev <donik.ganiev05@mail.ru>
2020-01-05 09:50:58 +0300

    tests: multiply test for nps

--- main_test.go
+++ main_test.go
@@ -3,11 +3,27 @@ package main
 import "testing"
 
 func Test_nps(t *testing.T) {
-
-	scores := []int{10, 7, 10, 10, 10}
+	/*scores := []int{10, 7, 10, 10, 10}
 	want := 80
 	got := nps(scores)
 	if  want!=got{
 		t.Error("nps with args:", scores,"want:",want,"got:",got)
+	}*/
+	testCases := []struct{
+		name string
+		scores []int
+		want int
+	}{
+		{"All promoters",[]int{10,10,10},100},
+		{"All detractors",[]int{0,0,0},-100},
+		{"All neutrals",[]int{8,8,8},0},
+		{"All mixed",[]int{10,10,8},66},
+
+	}
+	for _,testCase :=range testCases{
+		got :=nps(testCase.scores)
+		if got != testCase.want{
+			t.Error("nps test:",testCase.name,"got:",got,"want:",testCase.want)
+		}
 	}
 }
